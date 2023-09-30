7f259a0f60d6a95f6e82d4da7bcfa83a203c61a5
Rodney E Mora-Escalante <rodney.moraescalante@gmail.com>
2023-09-29 19:47:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy import pi
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 
 ## JONSWAp spectrum
 def JONSWAP_HsTp(f, Tp = 10, Hs = 1.5, gamma = 3.3):
@@ -62,12 +64,76 @@ DS = np.zeros((len(f), len(dirs)))
 for ii in range(0, len(Hs)):
   DS = DS + DirSpec_HsTp(f, dirs, Hs[ii], Tp[ii], dirp[ii], spr[ii], 3.3, model = 'D', sf = 'cos2s')
 
+##
+## Color palette to use in contourf or pcolor
+#1. White
+Whi = ['#FFFFFF']
+#2. Pinks to Blues
+PinBlu = ['#C9BFCD', '#B19FBB', '#A78DB4', '#A480B3', '#A470B8', '#B34AD7', '#C50EFB', '#AE0AFE']#, '#9415FE', '#7320FD']
+#3. Blues to Greens
+BluGree = ['#562BFD', '#3C35FC', '#1F3FFC', '#004CFC', '#0070F9', '#008EF7', '#02AAF4', '#03CDF2', '#03EBF0']
+#4. Greens to Yellows
+GreeYell = ['#25ED5B', '#57F846', '#7BFF36', '#94FE2C', '#ABFF21', '#C7FE15', '#DCFF0E', '#F2FF03', '#F4F105']
+#5. Orange to Reds
+OraRed = ['#F7E205', '#FAD305', '#FDC600', '#FFB800', '#FFA900', '#FF9B00', '#FF7C01', '#FF3E00', '#FF0000']#, '#E50001']
+
+colors_palette = Whi + PinBlu + BluGree + OraRed
+
+## Define Log Levels
+bounds1 = np.arange(0.0001, 0.001, 0.0001)
+bounds2 = np.arange(0.001, 0.01, 0.001)
+bounds3 = np.arange(0.01, 0.1, 0.01)
+bounds4 = np.arange(0.1, 1, 0.1)
+bounds5 = np.arange(1, 11, 1)
+levels = np.hstack([bounds2, bounds3, bounds4])
+
+## Parameters for colorbar
+cmap = colors.LinearSegmentedColormap.from_list('mymap', colors_palette)
+cmap = colors.ListedColormap(cmap(np.linspace(0, 1, 27)))
+# Logaritmic scale colors
+norm = colors.BoundaryNorm(np.log(levels), cmap.N, clip = True)
+
 ## Make plot
 fig, ax = plt.subplots(figsize = (10, 6), facecolor = 'w',
-           edgecolor = 'k', constrained_layout = True)
+           edgecolor = 'k', constrained_layout = True, subplot_kw = dict(polar = True))
 
-hc1 = ax.contourf(dirs, f, DS, cmap = cmap0)
-    ## Colorbar
-    #hc1.set_clim(0, 0.2)
-    cb = fig.colorbar(hc1, ax = ax)
-    cb.set_label(r'$m^2/Hz/rad$')
+hc1 = ax.pcolormesh(dirs*pi/180, f, np.log(DS), norm = norm, cmap = cmap, shading = 'gouraud')
+
+## Radii and angles labels
+ax.set_theta_zero_location('N')
+
+## Clockwise
+ax.set_theta_direction(-1)
+ax.set_thetagrids(np.arange(0, 360, 30),\
+			labels = [str(i)+'º' if i > 0 else str(i)+'ºN' for i in range(0, 360, 30)],
+			fontsize = 8, fontweight = 'bold')
+
+## Frequency space
+if False:
+	ax.set_rmax(0.5)
+	ax.set_rgrids([0.05, 0.1, 0.2, 0.3, 0.4],\
+				labels = [r'0.05 Hz', r'0.1 Hz', r'0.2 Hz',\
+				r'0.3 Hz', r'0.4 Hz'],\
+				fontweight = 'bold', fontsize = 6)
+## Period space
+if True:
+	ax.set_rmax(12)
+	ax.set_rgrids([2, 4, 6, 8, 10],\
+				labels = [r'2', r'4', r'6', r'8',\
+				r'10s'], angle = 45,\
+				fontweight = 'bold', fontsize = 6)
+
+ticklabel = [r'$10^{-3}$', r'$10^{-2}$',\
+			r'$10^{-1}$', r'$10^0$']
+cbar = fig.colorbar(hpc, ax = axs, shrink = 0.8, pad = 0.01, spacing = 'proportional',\
+ticks = np.log([0.001, 0.01, 0.1, 1]))
+cbar.set_ticklabels(ticklabel)
+cbar.set_label(r'$\log_{10}[(S(f,\theta)/S(f,\theta)^{max}]$')
+
+## Legend and grid
+ax.grid(True, linestyle = '--', alpha = .5, color = 'black',linewidth = .5)
+
+display(fig, target="mpl")
+## Colorbar
+cb = fig.colorbar(hc1, ax = ax)
+cb.set_label(r'$m^2/Hz/rad$')
